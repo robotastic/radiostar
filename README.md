869aba1907ab803f2a507d20d9be888877c5dad5
Luke Berndt <luke@robotastic.com>
2021-10-26 11:53:53 -0400

    Uploads to Blob!

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 `docker run -v ${PWD}:/app -it radiostar:dev /bin/bash`  
 
 `docker run --privileged --ulimit core=-1 -v ${PWD}:/app -v /dev/bus/usb:/dev/bus/usb  -v /var/run/dbus:/var/run/dbus -v /var/run/avahi-daemon/socket:/var/run/avahi-daemon/socket -it radiostar:dev /bin/bash` 
+
+`docker exec -it radiostar:dev /bin/bash`
--- fmrx_nogui.py
+++ fmrx_nogui.py
@@ -10,16 +10,6 @@
 
 from distutils.version import StrictVersion
 
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
-
 from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import gr
@@ -38,7 +28,7 @@ class fmrx(gr.top_block):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet")
-
+        self.log = gr.logger("log_debug")
 
         ##################################################
         # Variables
@@ -49,17 +39,15 @@ class fmrx(gr.top_block):
         # Blocks
         ##################################################
         self.rtlsdr_source_0 = osmosdr.source("rtl=200")
-        #self.rtlsdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
         self.rtlsdr_source_0.set_sample_rate(samp_rate)
         self.rtlsdr_source_0.set_center_freq(88500000, 0)
-        #self.rtlsdr_source_0.set_freq_corr(0, 0)
         self.rtlsdr_source_0.set_gain(30, 0)
-        #self.rtlsdr_source_0.set_if_gain(20, 0)
-        #self.rtlsdr_source_0.set_bb_gain(20, 0)
-        #self.rtlsdr_source_0.set_antenna('', 0)
         self.rtlsdr_source_0.set_bandwidth(200000, 0)
+
         self.blocks_wavfile_sink_0 = blocks.wavfile_sink('/app/test.wav', 1, 48000, blocks.FORMAT_WAV, blocks.FORMAT_PCM_16)
+        
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(1)
+        
         self.analog_fm_demod_cf_0 = analog.fm_demod_cf(
         	channel_rate=240000,
         	audio_decim=5,
@@ -69,9 +57,10 @@ class fmrx(gr.top_block):
         	gain=1.0,
         	tau=75e-6,
         )
-        #authentication_method="url_with_sas",
+        
         self.blob_sink = blob_sink(
-            url="https://radiostar.blob.core.windows.net/radiostar-sdr",
+            authentication_method="url_with_sas",
+            url="SAS URL goes here!",
             blob_name="radiostar-sdr",
             container_name="radiostar"
         )
@@ -94,15 +83,20 @@ class fmrx(gr.top_block):
         self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
 
     def closeEvent(self, event):
+        self.log.debug("closeEvent()")
         event.accept()
+
 
 def main(top_block_cls=fmrx, options=None):
     tb = top_block_cls()
-    
+    log = gr.logger("log_debug")    
     tb.start()
+
     time.sleep(10)
+
     tb.stop()
 
+    tb.wait()
 
 
 if __name__ == '__main__':
